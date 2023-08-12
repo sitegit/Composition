3b2411e467af6c848a84c6418c6757b514fd5512
Alexandr Pavlov <pavlov549@gmail.com>
2023-08-12 17:27:40 +0300

    create welcome fragment

--- app/src/main/java/com/example/composition/presentation/screens/level/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/composition/presentation/screens/level/ChooseLevelFragment.kt
@@ -1,18 +1,30 @@
 package com.example.composition.presentation.screens.level
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.composition.R
+import androidx.fragment.app.Fragment
+import com.example.composition.databinding.FragmentChooseLevelBinding
+
 class ChooseLevelFragment : Fragment() {
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding: FragmentChooseLevelBinding
+        get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding == null")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+    ): View {
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        fun newInstance() = ChooseLevelFragment()
+    }
 }
--- app/src/main/java/com/example/composition/presentation/screens/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/example/composition/presentation/screens/welcome/WelcomeFragment.kt
@@ -1,20 +1,46 @@
 package com.example.composition.presentation.screens.welcome
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.composition.R
+import com.example.composition.databinding.FragmentWelcomeBinding
+import com.example.composition.presentation.screens.level.ChooseLevelFragment
 
 class WelcomeFragment : Fragment() {
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding: FragmentWelcomeBinding
+        get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding == null")
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonUnderstand.setOnClickListener {
+            launchChooseLevelFragment()
+        }
+    }
+
+    private fun launchChooseLevelFragment() {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, ChooseLevelFragment.newInstance())
+            .addToBackStack(null)
+            .commit()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
