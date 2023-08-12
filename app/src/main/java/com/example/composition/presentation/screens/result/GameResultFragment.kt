584ccaf1c0148951d3c34a155642f6cc8d140685
Alexandr Pavlov <pavlov549@gmail.com>
2023-08-12 15:03:39 +0300

    create fragments design

---
+++ app/src/main/java/com/example/composition/presentation/screens/result/GameResultFragment.kt
@@ -0,0 +1,19 @@
+package com.example.composition.presentation.screens.result
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.composition.R
+class GameResultFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game_result, container, false)
+    }
+
+}
